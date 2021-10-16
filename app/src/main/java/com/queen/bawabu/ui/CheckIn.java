50c802278f138726efc2f7462fafc7336b4430a2
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-10-16 16:14:56 +0300

    made changes to check in

--- app/src/main/java/com/queen/bawabu/ui/CheckIn.java
+++ app/src/main/java/com/queen/bawabu/ui/CheckIn.java
@@ -2,15 +2,44 @@ package com.queen.bawabu.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.queen.bawabu.R;
 
 public class CheckIn extends AppCompatActivity {
 
+    ImageView imageView;
+    Button mReg;
+    EditText mName, mPlate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
+
+        imageView = findViewById(R.id.image);
+        mReg = findViewById(R.id.reg);
+
+        mReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckIn.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(CheckIn.this, Fingerprint.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
