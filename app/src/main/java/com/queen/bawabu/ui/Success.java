6c17738acb75c0198f50d0e1b1947b45cb162166
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-10-17 15:08:07 +0300

    added content to success file

--- app/src/main/java/com/queen/bawabu/ui/Success.java
+++ app/src/main/java/com/queen/bawabu/ui/Success.java
@@ -2,15 +2,39 @@ package com.queen.bawabu.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.queen.bawabu.R;
 
 public class Success extends AppCompatActivity {
+    SharedPreferences newPreference;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        TextView result = findViewById(R.id.info_text);
+        Button Chkout = findViewById(R.id.LogOut);
+
+        newPreference = getSharedPreferences("user_details", MODE_PRIVATE);
+        intent =new Intent(Success.this, CheckIn.class);
+        result.setText("Welcome" + newPreference.getString("Name", null));
+
+        Chkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor edit = newPreference.edit();
+                edit.clear();
+                edit.commit();
+                startActivity(intent);
+            }
+        });
     }
 }
